6bddb2cfe6be8e1f9cd5db0157e6d4df4b7e38ca
mrpumpkin98 <sju4486@naver.com>
2023-05-17 13:52:04 +0900

    “배포연습”

--- src/components/commons/modals/writePostModal/Modal/modal.tsx
+++ src/components/commons/modals/writePostModal/Modal/modal.tsx
@@ -10,20 +10,11 @@ const Modal: React.FC<ModalProps> = ({ onClose, children }) => {
     <S.ModalWrapper>
       {children}
       <S.Wrapper>
-        <S.TitleWrapper>
-          <S.Title>서비스</S.Title>
-          <S.Close onClick={onClose} />
-        </S.TitleWrapper>
-        <S.DivideLine />
         <S.ContentsWrapper>
-          <S.Contents>서비스 전체</S.Contents>
-          <S.Contents>레슨</S.Contents>
-          <S.Contents>홈/리빙</S.Contents>
-          <S.Contents>이벤트</S.Contents>
-          <S.Contents>비즈니스</S.Contents>
-          <S.Contents>디자인/개발</S.Contents>
-          <S.Contents>건강/미용</S.Contents>
-          <S.Contents>기타</S.Contents>
+          <S.Contents>클래스 생성하기</S.Contents>
+          <S.Contents>클래스 광고하기</S.Contents>
+          <S.Contents>사랑방 글쓰기</S.Contents>
+          <S.Contents>예약 관리</S.Contents>
         </S.ContentsWrapper>
       </S.Wrapper>
     </S.ModalWrapper>
