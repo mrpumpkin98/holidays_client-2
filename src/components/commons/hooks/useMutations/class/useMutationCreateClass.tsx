2daadc977a7a065c092a92d4aeaa17ecb1fc5a2d
siyeongE <dbtldud99@naver.com>
2023-05-18 12:52:19 +0900

    feat: 클래스 작성 구현

---
+++ src/components/commons/hooks/useMutations/class/useMutationCreateClass.tsx
@@ -0,0 +1,97 @@
+import { gql, useMutation } from "@apollo/client";
+import { useRouter } from "next/router";
+
+export const CREATE_CLASS = gql`
+  mutation createClass($createClassInput: CreateClassInput!) {
+    createClass(createClassInput: $createClassInput) {
+      class_id
+      title
+      content_summary
+      price
+      class_mNum
+      address
+      address_detail
+      category
+      address_category
+      total_time
+      content
+      accountNum
+      accountName
+      bankName
+      is_ad
+    }
+  }
+`;
+
+interface IFormCreateClassData {
+  title: string;
+  content_summary: string;
+  price: number;
+  class_mNum: number;
+  address: string;
+  address_detail: string;
+  category: string;
+  address_category: string;
+  total_time: string;
+  content: string;
+  accountNum: string;
+  accountName: string;
+  bankName: string;
+  date: string;
+  remain: string;
+
+  images?: string[];
+}
+
+export const UseMutationCreateClass = () => {
+  const [createClass] = useMutation(CREATE_CLASS);
+
+  // 등록하기 버튼
+  const onClickSubmit = async (data: IFormCreateClassData) => {
+    const router = useRouter();
+
+    console.log("등록하기 버튼 누름");
+    try {
+      const result = await createClass({
+        variables: {
+          createClassInput: {
+            title: data.title,
+            content_summary: data.content_summary,
+            price: String(data.price),
+            // price: parseInt(data.price),
+            class_mNum: String(data.class_mNum),
+            // class_mNum: parseInt(data.class_mNum),
+            address: data.address,
+            address_detail: data.address_detail,
+            category: data.category,
+            address_category: data.address_category,
+            total_time: data.total_time,
+            content: data.content,
+            accountNum: data.accountNum,
+            accountName: data.accountName,
+            bankName: data.bankName,
+            classShedulesInput: {
+              date: data.date,
+              remain: data.remain,
+            },
+            imageInput: {
+              // url,
+              // type,
+              // is_main,
+            },
+          },
+        },
+      });
+
+      alert("클래스 등록에 성공하였습니다.");
+
+      // 클래스 디테일 페이지로 이동
+      const class_id = result.data?.createClass.class_id;
+      void router.push(`/classPage/${class_id}`);
+    } catch (error) {
+      if (error instanceof Error) alert(error.message);
+    }
+  };
+
+  return { onClickSubmit };
+};
--- src/components/units/classPage/write/classWrite.index.tsx
+++ src/components/units/classPage/write/classWrite.index.tsx
@@ -7,7 +7,36 @@ import { Modal, Upload } from "antd";
 import type { RcFile, UploadProps } from "antd/es/upload";
 import type { UploadFile } from "antd/es/upload/interface";
 
+// import MultipleDatePicker from "react-multiple-datepicker";
+import DaumPostcodeEmbed from "react-daum-postcode"; //  우편번호
+import type { Address } from "react-daum-postcode";
+import { UseMutationCreateClass } from "../../../commons/hooks/useMutations/class/useMutationCreateClass";
+
 export default function ClassWrite() {
+  // 우편 번호
+  const [isOpen, setIsOpen] = useState(false);
+
+  // 우편번호 모달창
+  const onToggleModal = (): void => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleComplete = (data: Address): void => {
+    onToggleModal();
+  };
+
+  // -------------
+
+  // // 달력
+  // const [selectedDates, setSelectedDates] = useState([]);
+
+  // const handleDateSelect = (dates: any) => {
+  //   setSelectedDates(dates);
+  //   console.log(dates);
+  // };
+
+  // -------------
+
   // 이미지 등록
   const getBase64 = (file: RcFile): Promise<string> =>
     new Promise((resolve, reject) => {
@@ -65,16 +94,7 @@ export default function ClassWrite() {
     </div>
   );
 
-  // 달력 크기 지정
-  const [size] = useState<SizeType>("large");
-  const [state, setState] = useState(); // 달력 값 담기
-
-  // 달력 onchange
-  const onChangeDate = (dates: any, dateStrings: [string, string]) => {
-    // setState(dateStrings);
-    console.log("Selected Range:", dates);
-    console.log("Range Strings:", dateStrings);
-  };
+  // -------------
 
   // 카테고리 onchange
   const onChangeCategory = (value: string) => {
@@ -91,9 +111,20 @@ export default function ClassWrite() {
     console.log("최대인원", value);
   };
 
+  // --------
+  // 등록하기 버튼
+  const { onClickSubmit } = UseMutationCreateClass();
+
   return (
     <>
       <S.Wrapper>
+        {/* 우편번호 모달 */}
+        {isOpen && (
+          <Modal open={isOpen} onOk={onToggleModal} onCancel={onToggleModal}>
+            <DaumPostcodeEmbed onComplete={handleComplete} />
+          </Modal>
+        )}
+
         <S.Wrapper_header>
           <S.Wrapper_header_left>신규 클래스 개설</S.Wrapper_header_left>
         </S.Wrapper_header>
@@ -111,16 +142,16 @@ export default function ClassWrite() {
               { value: "4", label: "요리" },
             ]}
           />
-          <S.Error>에러</S.Error>
+          {/* <S.Error>에러</S.Error> */}
           <S.Label>클래스 이름을 입력해주세요</S.Label>
           <S.TextInput type="text" placeholder="클래스 이름을 입력해주세요" />
-          <S.Error>에러</S.Error>
+          {/* <S.Error>에러</S.Error> */}
           <S.Label>클래스 한줄요약을 입력해주세요</S.Label>
           <S.TextInput
             type="text"
             placeholder="클래스 한줄요약을 입력해주세요"
           />
-          <S.Error>에러</S.Error>
+          {/* <S.Error>에러</S.Error> */}
           <S.Label>대표 이미지를 올려주세요</S.Label>
           <Upload
             action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
@@ -143,7 +174,7 @@ export default function ClassWrite() {
             <S.Img />
             <S.Img />
           </S.Img_box> */}
-          <S.Error>에러</S.Error>
+          {/* <S.Error>에러</S.Error> */}
           <S.Wrapper_body_middle>
             <S.Wrapper_body_middle_left>
               <S.Label>클래스 소요 시간을 입력해주세요</S.Label>
@@ -159,9 +190,8 @@ export default function ClassWrite() {
                   { value: "4시간", label: "4시간" },
                 ]}
               />
-              <S.Error>에러</S.Error>
+              {/* <S.Error>에러</S.Error> */}
             </S.Wrapper_body_middle_left>
-
             <S.Wrapper_body_middle_right>
               <S.Label>클래스 최대 인원을 선택해주세요</S.Label>
               <S.Number
@@ -171,9 +201,12 @@ export default function ClassWrite() {
                 defaultValue={1}
                 // onChange={onChangeNumber}
               />
-              <S.Error>에러</S.Error>
+              {/* <S.Error>에러</S.Error> */}
             </S.Wrapper_body_middle_right>
           </S.Wrapper_body_middle>
+          <S.Label>클래스 가격을 입력해주세요</S.Label>
+          <S.TextInput type="int" placeholder="숫자만 입력해주세요" />
+          {/* <S.Error>에러</S.Error> */}
           <S.Label>클래스 위치를 입력해주세요</S.Label>
           <S.Wrapper_body_map>
             <S.Map>map</S.Map>
@@ -181,7 +214,9 @@ export default function ClassWrite() {
             <S.Wrapper_body_map_right>
               <S.Wrapper_body_map_right_top>
                 <S.AddressInput />
-                <S.AddressBtn>우편번호 검색</S.AddressBtn>
+                <S.AddressBtn onClick={onToggleModal}>
+                  우편번호 검색
+                </S.AddressBtn>
               </S.Wrapper_body_map_right_top>
 
               <S.Wrapper_body_map_right_bottom>
@@ -190,21 +225,53 @@ export default function ClassWrite() {
               </S.Wrapper_body_map_right_bottom>
             </S.Wrapper_body_map_right>
           </S.Wrapper_body_map>
-          <S.Error>에러</S.Error>
+          {/* <S.Error>에러</S.Error> */}
           <S.Label>클래스 세부내용을 입력해주세요</S.Label>
           <S.Contents />
-          <S.Error>에러</S.Error>
-          <S.Label>클래스 일정을 선택해주세요</S.Label>
-          <S.Spaced direction="vertical">
-            <S.RangePicker size={size} onChange={onChangeDate} />
-          </S.Spaced>
-          {/* <div>{state}</div> */}
-          <S.Error>에러</S.Error>
+          {/* <S.Error>에러</S.Error> */}
+          <S.Label>클래스 일정을 선택주해세요</S.Label>
+          {/* ------------ */}
 
-          <div>
+          {/* 달력 */}
+          {/* <MultipleDatePicker
+            onSubmit={handleDateSelect}
+            minDate={new Date()} // 현재보다 이전 날짜 선택 불가
+          />
+
+          <S.DatelistWrapper>
+            {selectedDates.map((date) => (
+              <S.Datelist key={date.toISOString()}>
+                {date.toLocaleDateString()}
+              </S.Datelist>
+            ))}
+          </S.DatelistWrapper> */}
+          {/* <S.Error>에러</S.Error> */}
+
+          {/* ------------ */}
+          {/* <S.Error>에러</S.Error> */}
+          <S.Label>입금 계좌</S.Label>
+          <S.TextInput type="int" placeholder="'-' 빼고 숫자만 입력해주세요." />
+          {/* <S.Error>에러</S.Error> */}
+          <S.BankWrapper>
+            <div>
+              <S.Label>예금주</S.Label>
+              <S.TextInput2 type="text" placeholder="예금주를 작성해주세요" />
+              {/* <S.Error>에러</S.Error> */}
+            </div>
+
+            <div>
+              <S.Label>입금 은행</S.Label>
+              <S.TextInput2
+                type="text"
+                placeholder="입금 은행을 작성해주세요"
+              />
+              {/* <S.Error>에러</S.Error> */}
+            </div>
+          </S.BankWrapper>
+          <S.BtnWrapper>
             <S.CancelBtn>취소</S.CancelBtn>
             <S.SubmitBtn>등록</S.SubmitBtn>
-          </div>
+          </S.BtnWrapper>
         </S.Wrapper_body>
       </S.Wrapper>
     </>
